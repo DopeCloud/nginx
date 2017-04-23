f49abf68654676dc26ab8f73dea6be9fa3ba7ecb
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-04-23 09:48:14 +0000

    Create static.sh

---
+++ static.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# STATIC = The bash script which will create virtualhost config for static page, which support only those langs.
+# ===========================================================
+# ===========================================================
+# 1. HTML.
+# 2. JAVASCRIPT.
+# 3. XML.
+# 4. CSS.
+# 5. (Langs will updated after sometime.)
+# ===========================================================
+# ===========================================================
+
+cfile(){
+       cp /store/nginx/static-html /nginx/live/$1
+       
+       
+       
+}
+
+res(){
+       
+       
+       
+       
+}
+
+
+# END.
