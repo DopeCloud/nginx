ed1185b03c6278beddc6e70680bd410af9545733
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-04-23 13:23:44 +0000

    Update static.sh

--- static.sh
+++ static.sh
@@ -11,19 +11,45 @@
 # ===========================================================
 # ===========================================================
 
-cfile(){
-       cp /store/nginx/static-html /nginx/live/$1
-       
-       
-       
+# ===========================================================
+# START EVERYTIME.
+# ===========================================================
+
+cdir() {
+       mkdir -p /home/$client/$domain/
+       cp -a /store/nginx/templates/public_html /home/$client/$domain/public_html
+       mkdir -p /home/$client/$domain/logs
+       mkdir -p /home/$client/$domain/ssl
+       mkdir -p /home/$client/$domain/cache
+
+       chown -R $client:$client /home/$client
+       chown -R $client:$client /home/$client/$domain
+       chown -R $client:$client /home/$client/$domain/public_html
+       chown -R $client:$client /home/$client/$domain/public_html/*
+       chown -R $client:$client /home/$client/$domain/logs
+       chown -R $client:$client /home/$client/$domain/ssl
+       chown -R $client:$client /home/$client/$domain/cache
+
+       chown -R www-data:www-data /home/$client/$domain
+       chown -R www-data:www-data /home/$client/$domain/public_html
+       chown -R www-data:www-data /home/$client/$domain/public_html/*
 }
 
-res(){
-       
-       
-       
-       
+# ===========================================================
+# END EVERYTIME.
+# ===========================================================
+
+# ===========================================================
+# START NON-SSL PROCCESS.
+# ===========================================================
+
+create() {
+       cp /store/nginx/templates/static-html-full /nginx/live/$domain
+       sed -r -i "s/changeme/$domain/g" /nginx/live/$domain
+       sed -r -i "s/theclient/$client/g" /nginx/live/$domain
+       service nginx reload
 }
 
-
-# END.
+# ===========================================================
+# END NON-SSL PROCCESS.
+# ===========================================================
